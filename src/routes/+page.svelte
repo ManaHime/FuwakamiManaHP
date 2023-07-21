7e550f656c66a8f40c48bfe9d533bc436f8da403
Fuwakami Mana <mana@fuwakami.jp>
2023-07-21 11:39:41 +0900

    Remove furigana because it conflicts with some accessibility.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
     <div class="flex w-full max-w-screen-xl p-5 place-content-between card">
         <div class="flex flex-col">
             <h1 class="h1">ウェブ開発、言語学習、映像編集の軌跡を紡ぐ</h1>
-            <h2　class="h2"><ruby>創造性<rt>そうぞうせい</rt></ruby>と<ruby>学<rt>まな</rt></ruby>びを<ruby>織<rt>お</rt></ruby>り<ruby>交<rt>ま</rt></ruby>ぜたウェブの<ruby>旅<rt>たび</rt></ruby></h2>
+            <h2　class="h2">創造性と学びを織り交ぜたウェブの旅</h2>
         </div>
         <div class="flex-1 max-w-full place-items-center">
             <img class="image drop-shadow shadow-black" src="https://cdn.discordapp.com/attachments/546519231601311761/1108732067296055366/Mana_2Dmodel_002.png"  alt="Mana's vtuber avatar">
