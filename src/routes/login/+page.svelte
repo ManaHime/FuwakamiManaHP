a9a9b4dac0410f1a72868cd3703573fffd6b7d30
Fuwakami Mana <mana@fuwakami.jp>
2023-07-23 11:06:02 +0900

    Updating formating.

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,20 +4,22 @@
     
     export let form: ActionData   
 </script>
-<div class="flex flex-wrap h-full place-content-center">
-    <div class="flex flex-row items-center h-full">
-        <div class="flex flex-col px-16 py-10 card">
-            <h1 class="text-xl text-center uppercase">Login</h1>
-            <form class="flex flex-col gap-3" method="POST" use:enhance>
-                <input name="email" type="email" placeholder="email@example.com" class="input">
-                <input name="password" type="password" placeholder="password" class="input">
-                {#if form?.invalid}
-                    <p class="error">Username and password is required.</p>
-                {/if}
-                {#if form?.credentials}
-                    <p class="error">You have entered the wrong credentials.</p>
-                {/if}
-                <button type="submit" class="btn">Login</button>
+<div class="flex flex-wrap h-full place-content-center 2xl:mr-52">
+    <div class="flex flex-row items-center h-full max-w-sm">
+        <div class="flex flex-col justify-around px-8 pb-8 card gap">
+            <h1 class="p-8 text-center uppercase h1">ログイン</h1>
+            <form class="flex flex-col gap-4" method="POST" use:enhance>
+                <div class="p-4 space-y-4 border border-surface-500 rounded-container-token">
+                    <input name="email" type="email" placeholder="email@example.com" class="rounded-md input">
+                    <input name="password" type="password" placeholder="パスワード" class="rounded-md input">
+                    {#if form?.invalid}
+                        <p class="error">Username and password is required.</p>
+                    {/if}
+                    {#if form?.credentials}
+                        <p class="error">You have entered the wrong credentials.</p>
+                    {/if}
+                </div>
+                <button type="submit" class="rounded-md btn variant-soft-primary">ログイン</button>
             </form>
         </div>
     </div>
