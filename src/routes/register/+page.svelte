bed5df0250c73d3bd9a57779618dd4119a4e4c19
Fuwakami Mana <mana@fuwakami.jp>
2023-07-23 11:15:14 +0900

    Updating formating

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -62,20 +62,24 @@
 
 </script>
 
-<div class="flex flex-wrap h-full place-content-center">
-    <div class="flex flex-col px-16 py-10 card">
-        <h2 class="pb-5 text-center uppercase h2">登録</h2>
-        <form class="flex flex-col gap-3" method="post" use:enhance>
-            <input class="text-center input" name="username" placeholder="ユーザーネーム" type="text" bind:value={username} required>
-            <input class="text-center input" name="email" placeholder="email@example.com" type="email" bind:value={email} required>
-            {#if form?.invalid}
-                <p class="error">Username is taken.</p>
-            {/if}
-            <input class="text-center input" name="password" placeholder="パスワード" type="password" bind:value={password} required>
-            <input class="text-center input" name="confirmPassword" placeholder="もう一回パスワード" type="password" bind:value={confirmPassword} required>
-            <label for="password-strength">Password strength: {strengthDescription}</label>
-            <meter class="w-full" id="password-strength" value={score} low="1.9" high="2.9" optimum="4" max="4" />
-            <button class="w-full btn variant-filled-primary" id="registerBtn" type="submit" disabled={formDisabled}>Register</button>
-        </form>
+<div class="flex flex-wrap h-full place-content-center 2xl:mr-52">
+    <div class="flex flex-row items-center h-full max-w-sm">
+        <div class="flex flex-col justify-around px-8 pb-8 card gap">
+            <h1 class="h1 text-center uppercase p-8">登録</h1>
+            <form class="flex flex-col gap-4" method="POST" use:enhance>
+                <div class="border border-surface-500 space-y-4 p-7 rounded-container-token">
+                    <input class="text-center input rounded-md" name="username" placeholder="ユーザーネーム" type="text" bind:value={username} required>
+                    <input class="text-center input rounded-md" name="email" placeholder="email@example.com" type="email" bind:value={email} required>
+                    {#if form?.invalid}
+                        <p class="error">Username is taken.</p>
+                    {/if}
+                    <input class="text-center input rounded-md" name="password" placeholder="パスワード" type="password" bind:value={password} required>
+                    <input class="text-center input rounded-md" name="confirmPassword" placeholder="もう一回パスワード" type="password" bind:value={confirmPassword} required>
+                    <label for="password-strength">Password strength: {strengthDescription}</label>
+                    <meter class="w-full" id="password-strength" value={score} low="1.9" high="2.9" optimum="4" max="4" />
+                </div>
+                <button class="w-full btn variant-filled-primary rounded-md mt-2" id="registerBtn" type="submit" disabled={formDisabled}>Register</button>
+            </form>
+        </div>
     </div>
 </div>
