9fceebf30c8ba89d7c29c5da2a835c5431f72cfa
Fuwakami Mana <mana@fuwakami.jp>
2023-07-27 11:14:14 +0900

    Remove unused text editor.

--- src/routes/(admin)/admin/new-post/+page.svelte
+++ src/routes/(admin)/admin/new-post/+page.svelte
@@ -2,56 +2,6 @@
 	import { enhance } from '$app/forms';
 	import Tiptap from '$lib/components/Tiptap.svelte';
 	let textContent: string;
-	// Rich Text Editor
-	// import { onMount, onDestroy } from 'svelte';
-	// import { Editor } from '@tiptap/core';
-	// import StarterKit from '@tiptap/starter-kit';
-	// import Link from '@tiptap/extension-link';
-
-	// let element: Element;
-	// let editor: Editor;
-	// let postTextBody: string;
-	// $: postTextBody = editor?.getHTML();
-	// onMount(() => {
-	// 	editor = new Editor({
-	// 		element: element,
-	// 		extensions: [StarterKit, Link],
-	// 		editorProps: {
-	// 			attributes: {
-	// 				class: 'textarea h-[300px] overflow-y-scroll overflow-clip posts prose dark:prose-invert'
-	// 			}
-	// 		},
-	// 		content: '',
-	// 		onTransaction: () => {
-	// 			// force re-render so `editor.isActive` works as expected
-	// 			editor = editor;
-	// 		}
-	// 	});
-	// });
-	// onDestroy(() => {
-	// 	if (editor) {
-	// 		editor.destroy();
-	// 	}
-	// });
-
-	// const setLink = () => {
-	// 	const previousUrl = editor.getAttributes('link').href;
-	// 	const url = window.prompt('URL', previousUrl);
-
-	// 	// cancelled
-	// 	if (url === null) {
-	// 		return;
-	// 	}
-
-	// 	// empty
-	// 	if (url === '') {
-	// 		editor.chain().focus().extendMarkRange('link').unsetLink().run();
-	// 		return;
-	// 	}
-
-	// 	// update link
-	// 	editor.chain().focus().extendMarkRange('link').setLink({ href: url }).run();
-	// };
 </script>
 
 <div class="flex flex-col max-w-md m-auto h-1/2">
