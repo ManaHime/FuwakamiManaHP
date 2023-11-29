3414d6ccbe6939af8c15eb30c5fdaebd8766095b
Fuwakami Mana <janik@hikarihq.com>
2023-11-29 17:25:57 -0500

    Begin adding discord schedule builder

---
+++ src/routes/[[lang]]/vtuber-schedule/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let elemCarousel: HTMLDivElement;
+	const imgIds = ['odder-otter', 'odder-otter-cafe-1', 'odder-otter-cafe-2'];
+	let scrollRightInterval: ReturnType<typeof setInterval>;
+
+
+</script>
+
+<div class="flex flex-col-reverse gap-5 place-items-center 2xl:mr-52">
+	<div class="w-full max-w-screen-lg p-5 prose xl:prose-2xl card">
+		<div class="flex flex-col place-content-between">
+			<form>
+				Mon <input type="time" name="" id=""> <input type="text" name="" id=""><br>
+				Tue <input type="time" name="" id=""> <input type="text" name="" id=""><br>
+				Wed <input type="time" name="" id=""> <input type="text" name="" id=""><br>
+				Thu <input type="time" name="" id=""> <input type="text" name="" id=""><br>
+				Fri <input type="time" name="" id=""> <input type="text" name="" id=""><br>
+				Sat <input type="time" name="" id=""> <input type="text" name="" id=""><br>
+				Sun <input type="time" name="" id=""> <input type="text" name="" id="">
+			</form>
+			<p><a href="https://discord.gg/FDymkDsaxk">Odder Otter Discord Server</a></p>
+		</div>
+	</div>
+</div>
