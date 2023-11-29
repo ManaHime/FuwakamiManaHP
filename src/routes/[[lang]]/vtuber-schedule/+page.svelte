f5239c8daec29f6728e8e0d692d2c1b062e634a0
Fuwakami Mana <janik@hikarihq.com>
2023-11-29 17:34:41 -0500

    Test text area for schedule builder

--- src/routes/[[lang]]/vtuber-schedule/+page.svelte
+++ src/routes/[[lang]]/vtuber-schedule/+page.svelte
@@ -3,14 +3,14 @@
 	const imgIds = ['odder-otter', 'odder-otter-cafe-1', 'odder-otter-cafe-2'];
 	let scrollRightInterval: ReturnType<typeof setInterval>;
 
-
+	let scheduleData
 </script>
 
 <div class="flex flex-col-reverse gap-5 place-items-center 2xl:mr-52">
 	<div class="w-full max-w-screen-lg p-5 prose xl:prose-2xl card">
 		<div class="flex flex-col place-content-between">
 			<form>
-				Mon <input type="time" name="" id=""> <input type="text" name="" id=""><br>
+				Mon <input type="time" name="" id="" bind:value={scheduleData.mon.time}> <input type="text" name="" id=""><br>
 				Tue <input type="time" name="" id=""> <input type="text" name="" id=""><br>
 				Wed <input type="time" name="" id=""> <input type="text" name="" id=""><br>
 				Thu <input type="time" name="" id=""> <input type="text" name="" id=""><br>
@@ -18,7 +18,9 @@
 				Sat <input type="time" name="" id=""> <input type="text" name="" id=""><br>
 				Sun <input type="time" name="" id=""> <input type="text" name="" id="">
 			</form>
-			<p><a href="https://discord.gg/FDymkDsaxk">Odder Otter Discord Server</a></p>
+			<textarea>
+				{scheduleData.mon.time}
+			</textarea>
 		</div>
 	</div>
 </div>
